b9504d5c4eb12e432f937584beedc7b3ae501de7
Loarderon <ma-wu@windowslive.com>
2020-07-10 22:16:05 +0200

    Guess that's 2nd push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import Todo from './Todo'
+
 import './App.css';
 
 function App() {
+  
+  const [todos, setTodos] = useState(['Kiss my gf', 'Take out trash', 'Work out:', 'Pushups ', 'Crunches', 'Sit-ups', 'Everyday coding']);
+  const [input, setinput] = useState('');
+  
+  const addTodo = (event) => {
+    // this will fire off when button is clicked
+    event.preventDefault();
+    console.log("it's working!");
+    setTodos([...todos, input]);
+    setinput(''); // clear up input after hitting button
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello World!</h1> 
+      <form>
+      <FormControl>
+      <InputLabel>What's your next task?</InputLabel>
+      <Input value={input} onChange={event => setinput(event.target.value)}/>
+      </FormControl>
+
+      <Button disabled={!input} type='submit' onClick={addTodo} variant="contained" color="primary">Add Todo</Button>
+      </form> 
+      <ul>
+        {todos.map(todo => (
+          <Todo text={todo}/>
+          // <li>{todo}</li>
+        ))}
+        </ul>
+          </div>
   );
 }
 
